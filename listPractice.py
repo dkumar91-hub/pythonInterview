b523092764186bec6f3d026a18150b3077821c5e
dilip <dilip1026@gmail.com>
2022-05-17 18:32:20 +0530

    list operations

--- listPractice.py
+++ listPractice.py
@@ -1,25 +1,33 @@
-medicine = ["paracetamol","pantopd","dettol"]
-babyMedicine = ["calpol","gastica","paracetamol"]
-kirana = ["rice","atta","oil","masala"]
-toiletary = ["bodywash","soap","harpic","surf"]
+subject = ["eng","hindi","maths","sci","sst"]
+sports = ["football","boxing","hockey"]
+sportskit=["bat","ball","gloves"]
+capital = ["patna","bhopal","chennai"]
+'''interview questions'''
 
-'''interview question'''
-'''1: add new element to list and add new list to a list'''
-'''2: insert an element to i'th location'''
-'''3: delete element from list'''
-'''4: find size of list'''
+'''1:-print elements of list'''
+'''2:-adding elements to a list'''
+'''3:-finding index of element in a list'''
+'''4:-adding elements to i'th location in a list'''
+'''5:-removing element from a list'''
+'''6:-to insert elements from list to other list'''
+'''7:-size of list'''
 
-'''1: add new element to list'''
-medicine.append("cinirest")
-# print(medicine)
-medicine.extend(babyMedicine)
-print(medicine)
-'''2: insert an element to i'th location'''
-medicine.insert(2,"evil")
-print(medicine)
-'''3: delete element from i'th location'''
-medicine.\
-"evil")
-'''4: find size of list'''
-l1 = len(medicine)
-print(l1)
+'''1:-print elements of list'''
+print(sports)
+subject.append("sanskrit")
+'''2:-adding elements to a list'''
+print(subject)
+'''3:-finding index of element in a list'''
+print(subject[3])
+'''4:-adding elements to i'th location in a list'''
+sports.insert(2,"cricket")
+print(sports)
+'''5:-removing element from a list'''
+capital.remove("bhopal")
+print(capital)
+'''6:-to insert elements from list to other list'''
+sports.extend(sportskit)
+print(sports)
+'''7:-size of list'''
+len(capital)
+print(len(capital))
