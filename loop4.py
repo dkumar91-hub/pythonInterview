a1de1d54ea4a96fa8cd768ad2e40686239328e1c
dilip <dilip1026@gmail.com>
2022-05-20 17:26:12 +0530

    loop operations 2

--- loop4.py
+++ loop4.py
@@ -12,3 +12,17 @@ for x in l2:
     if x<l:
         l=x
 print(l)
+l4=["abc","efg","hij","abc","hij"]
+abc=[]
+efg=[]
+hij=[]
+for x in l4:
+    if x==("abc"):
+        abc.append(x)
+    elif x==("efg"):
+        efg.append(x)
+    else:
+        hij.append(x)
+print(len(abc))
+print(len(efg))
+print(len(hij))
