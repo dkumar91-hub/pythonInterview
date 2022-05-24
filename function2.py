fb139710b35775db2a8e1df436f305bc4bcdbc33
dilip <dilip1026@gmail.com>
2022-05-24 14:44:02 +0530

    loop operations 2

--- function2.py
+++ function2.py
@@ -21,3 +21,20 @@ def xyz(L1):
         print(i)
 list1 = [3,7,9,8]
 xyz(list1)
+def abc(l2):
+    for x in l2:
+        print(l2[3])
+list2 = [5,6,7,3]
+abc(list2)
+def pqr(l3):
+    for i in l3:
+        list2.append(list3)
+        list2.insert(2,5)
+        list2.extend(list3)
+        list2.remove(4)
+        print(len(list3))
+        print(list2)
+list3 = [9,1,3,8,5,4,2]
+list2 = [5,6,7,3]
+abc(list2)
+pqr(list3)
