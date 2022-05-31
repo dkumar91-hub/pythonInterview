63412fef0e80fbaa6b31094aa4697863526d57bf
dilip <dilip1026@gmail.com>
2022-05-31 15:27:17 +0530

    loop operations 2

---
+++ practiceFile 5.py
@@ -0,0 +1,28 @@
+def area_of_rectangle(l,b):
+    area_of_rectangle = l*b
+    print(area_of_rectangle)
+l=10
+b=40
+area_of_rectangle(l,b)
+
+def area_of_triangle(b,h):
+    area_of_triangle = .5*b*h
+    print(area_of_triangle)
+b=15
+h=20
+area_of_triangle(b,h)
+
+def area_of_circle(pi,r):
+    area_of_circle = pi*r*r
+    print(area_of_circle)
+pi=3.14
+r=5
+area_of_circle(pi,r)
+
+def average(sum_of_number,total_number_of_observation):
+    average =sum_of_number/total_number_of_observation
+    print(average)
+sum_of_number=405
+total_number_of_observation=5
+average(sum_of_number,total_number_of_observation)
+
